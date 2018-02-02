12b3efb1c64a96488a63cf8e0bb653c393cb6624
crocchi <crocchi@ROCCHI.gruppoangelini.intranet>
2018-02-02 15:19:48 +0100

    terza modifica

--- testGit/src/testGit/FirstClass.java
+++ testGit/src/testGit/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 		// TODO Auto-generated method stub
          System.out.println("This is the first class");
          System.out.println("And now I did a change");
+         System.out.println("And now I did another change");
 	}
 
 }
