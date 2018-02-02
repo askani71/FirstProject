29e347b91d01465d7ef0fef26d6e2e540eb7d8cd
crocchi <crocchi@ROCCHI.gruppoangelini.intranet>
2018-02-02 15:03:38 +0100

    Questo è il primo commit su Eclipse

---
+++ testGit/src/testGit/FirstClass.java
@@ -0,0 +1,11 @@
+package testGit;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("This is the first class");
+         System.out.println("And now I did a change");
+	}
+
+}
